a027153b8b98218cde04450c391e458d2e5ed747
MykolaDiachuk <diachuk.mykola2005@gmail.com>
2024-05-08 11:42:11 +0300

    add HttpServer

--- src/main/java/org/example/bot/Application.java
+++ src/main/java/org/example/bot/Application.java
@@ -9,13 +9,20 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException, IOException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
             log.info("Registering bot...");
@@ -26,6 +33,21 @@ public class Application {
         log.info("Telegram bot is ready to accept updates from user......");
 
         GameSessions.setGameSessions(InitializationFromDB.getGameSessionslist());
-
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8888"));
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(Executors.newCachedThreadPool());
+        System.out.println("Listening on port " + port + ".");
+        server.start();
+    }
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello world!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
